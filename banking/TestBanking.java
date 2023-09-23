fa4dc0316ce7794f1eb78a510857de5400ee4fb2
banber0 <134239676+banber0@users.noreply.github.com>
2023-09-23 15:25:52 +0800

    Update TestBanking.java

--- banking/TestBanking.java
+++ banking/TestBanking.java
@@ -8,7 +8,7 @@ public class TestBanking {
             Account account;
             //Creat an account that can has a 500.00 balance.
             System.out.println("Creat an account that can has a 500.00 balance");
-            customer = new Customer("Liu","zexu");
+            customer = new Customer("zexu","Liu");
             customer.setAccount(new Account(500.00));
             account = customer.getAccount();
             //Perform some account transactions
@@ -17,6 +17,6 @@ public class TestBanking {
             System.out.println("Withdraw 47.62:" + account.withdraw(47.62));
             System.out.println("Withdraw 400.00:" + account.withdraw(400.00));
             //print out final account balance
-            System.out.println("Customer[" + customer.getLastName() + "£¬" + customer.getFirstName() + "] has a balance of " + account.getBalance());
+            System.out.println("Customer[" + customer.getLastName() + "Â£Â¬" + customer.getFirstName() + "] has a balance of " + account.getBalance());
         }
     }
